9de5797899d61106059f392161dfa3d571be9f6f
Geek-zy <zy251021@163.com>
2016-12-21 17:24:40 +0800

    Ubuntu命令行播放音乐

---
+++ Ubuntu_Music/Ubuntu_Music.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "
+      +---------------+
+      |               |
+      |　请欣赏音乐 　|
+      |               |
+      +---------------+               
+"
+#Ubuntu 系统自带
+#paplay /usr/share/sounds/ubuntu/stereo/phone-incoming-call.ogg
+paplay ogg/Radiation.ogg
